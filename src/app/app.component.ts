9cf11244776fefd18d9426efb8e0b4a551ed708a
Rania benOtman <rawan_ben_2002@yahoo.com>
2023-09-02 15:13:54 +0200

    first lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { TmplAstHoverDeferredTrigger } from '@angular/compiler';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   title = 'angular-vedio-series';
+  todoValue!: string;
+  list!: Todo[];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+  onKey(event:any){
+    this.todoValue =  event
+  }
+  addItem() {
+  
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      }
+      this.list.push(newItem);
+    }
+    this.todoValue = ""
+  }
+  deleteItem(id: number) {
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
